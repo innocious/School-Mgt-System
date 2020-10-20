949bb7ff00861baddcb2967d0dd4838f10d587c1
Bliss911 <innociousbliss@gmail.com>
2020-10-20 02:26:42 +0100

    Fixed my admin table seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+// use App\Models\Admin;
+use Illuminate\Support\Str;
 
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +18,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('admins')->insert([
+            'admin_name' => 'Admin',
+            'admin_email' => 'test@example.com',
+            'admin_password' => bcrypt('password'),            
+        ]);
         
     }
 }
